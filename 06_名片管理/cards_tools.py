ad20c508b49fed0545feac43163cb564e6e8ae6f
gaoshang18 <gaoshang18@qq.com>
2019-03-26 20:17:54 +0800

    first commit “ “

---
+++ 06_名片管理/cards_tools.py
@@ -0,0 +1,30 @@
+def show_menu():
+    """显示菜单"""
+    print("*" * 50)
+    print("欢迎使用【名片管理系统】V1.0")
+    print("")
+    print("1.新增名片")
+    print("2.显示全部")
+    print("3.新搜索名片")
+    print("")
+    print("0.是退出")
+    print("")
+    print("*" * 50)
+
+
+def new_card():
+    """新增名片"""
+    print("-" * 50)
+    print("新增名片")
+
+
+def show_all():
+    """显示所有名片"""
+    print("-" * 50)
+    print("显示所有名片")
+
+
+def search_card():
+    """搜索名片"""
+    print("-" * 50)
+    print("搜索名片")
